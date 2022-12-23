601885fdffecdc76047a37f14b817675a13fb957
Eric Thornton <ewthornton@gmail.com>
2022-12-23 09:48:03 -0500

    change LED pin

--- LNB_Tone/src/main.cpp
+++ LNB_Tone/src/main.cpp
@@ -2,7 +2,7 @@
 
 int main() {
   DDRB |= (1<<PB0); //PB0 (pin 5) as clock output
-  DDRB |= (1<<PB1); //PB1 (pin 6) as LED output
+  DDRB |= (1<<PB4); //PB4 (pin 3) as LED output
 
   TCCR0A |= (1 << COM0A0); //toggle OC0A (pin 6)
   TCCR0A |= (1 << WGM01); //CTC mode
@@ -12,7 +12,7 @@ int main() {
   // fOCnx = fclk / (2 * N * (1+OCRnx))
   // 1,000,000 = (2 * 1 * (1 + OCR0A))
   OCR0A = 22; //21.74khz
-  PORTB |= (1<<PB1);
+  PORTB |= (1<<PB4);
 
   while(1) {
 
